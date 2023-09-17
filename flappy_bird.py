7fff21a9e38420ebb7149ab4d9482fedd9f46643
OkeLDF <112588985+OkeLDF@users.noreply.github.com>
2023-09-17 03:10:48 -0300

    Adicionado um obstáculo a mais
    Agora temos o dobro de obstáculos. Antes havia um longo espaço entre um obstáculo e outro; agora, temos um obstáculo a mais para evitar essa folga

--- flappy_bird.py
+++ flappy_bird.py
@@ -5,37 +5,43 @@ import random as rd
 # constantes
 gravity = 0.9
 jump = -10
-hole_size = 200
-obstacle_width = 80
 
 class Obstacle:
-    rect=0
-    lefttop=0
-    width_height=0
-    x1=0
-    y1=0
-    x2=0
-    y2=0
-    width=0
-    heigth=0
-    color='purple'
+    superior_pillar = 0
+    inferior_pillar = 0
+    color = ''
+    width = 0
+    hole_size = 0
 
-    def __init__(self, x:float, y:float, width:float, height:float, color='purple'):
-        self.rect = pg.Rect(x, y, width, height)
-        self.lefttop = pg.Vector2(x,y)
-        self.width_height = pg.Vector2(width,height)
-
-        self.x1 = x
-        self.y1 = y
-        self.x2 = x + width
-        self.y2 = y + height
-
-        self.width = width
-        self.heigth = height
+    def __init__(self, screen_x:float, screen_y:float, obstacle_width=80, hole_size=200, color='purple'):
         self.color = color
+        self.hole_size = hole_size
+        self.width = obstacle_width
+        self.superior_pillar = pg.Rect(screen_x, 0, obstacle_width, 0)
+        self.inferior_pillar = pg.Rect(screen_x, hole_size, obstacle_width, screen_y)
+    
+    def addto_x(self, value:float):
+        self.superior_pillar.left += value
+        self.inferior_pillar.left += value
+
+    def set_x(self, value:int):
+        self.superior_pillar.left = value
+        self.inferior_pillar.left = value
+
+    def set_y(self, value:int):
+        self.superior_pillar.update(self.superior_pillar.left, self.superior_pillar.top, self.superior_pillar.width, value)
+        self.inferior_pillar.update(self.inferior_pillar.left, value + self.hole_size, self.inferior_pillar.width, self.inferior_pillar.height)
+
+    def randomize_in_right(self, screen_width):
+        self.set_x(screen_width)
+        self.set_y(rd.choice(range(0,450)))
+
+    def get_pillars(self):
+        return [self.superior_pillar, self.inferior_pillar]
 
     def draw(self, screen):
-        pg.draw.rect(screen, self.color, self.rect, 10)
+        pg.draw.rect(screen, self.color, self.superior_pillar, 10)
+        pg.draw.rect(screen, self.color, self.inferior_pillar, 10)
 
 class Player:
     coordinate = 0
@@ -50,49 +56,52 @@ class Player:
         self.velocity = 0
 
     def touching(self, obstacle:Obstacle):
-        is_between_x1_and_x2 = self.coordinate.x + self.radius > obstacle.x1 and self.coordinate.x - self.radius < obstacle.x2
-        is_between_y1_and_y2 = self.coordinate.y + self.radius > obstacle.y1 and self.coordinate.y - self.radius < obstacle.y2
+        for pillar in obstacle.get_pillars():
+            is_between_x1_and_x2 = self.coordinate.x + self.radius > pillar.left and self.coordinate.x - self.radius < pillar.right
+            is_between_y1_and_y2 = self.coordinate.y + self.radius > pillar.top and self.coordinate.y - self.radius < pillar.bottom
         
-        if is_between_x1_and_x2 and is_between_y1_and_y2:
-            return True
+            if is_between_x1_and_x2 and is_between_y1_and_y2:
+                return True
         return False
     
     def draw(self, screen):
         pg.draw.circle(screen, self.color, self.coordinate, self.radius)
 
 pg.init()
-running = True
 screen = pg.display.set_mode((620, 720))
-y_center = screen.get_height()/2
+screen_y_center = screen.get_height()/2
 clock = pg.time.Clock()
 pg.display.set_caption("Flappy Bird in Python")
 
-#variáveis
-obstacle_x = 0
-obstacle_y = rd.choice(range(0,450))
+player = Player(310, screen_y_center, 30)
+obstacles = [
+    Obstacle(screen.get_width(), screen.get_height()),
+    Obstacle(screen.get_width(), screen.get_height())
+]
 
-player = Player(310, y_center, 30)
+def new_game(sleep_one_sec=True):
+    if sleep_one_sec: time.sleep(1)
 
-def new_game():
-    time.sleep(1)
-
-    global obstacle_y
-    global obstacle_x
+    global obstacles
     global player
-    obstacle_y = rd.choice(range(0,450))
-    obstacle_x = 0
-    player.coordinate.y = y_center
+    player.coordinate.y = screen_y_center
     player.velocity = 0
+    obstacles[0].randomize_in_right(screen.get_width())
+    obstacles[1].randomize_in_right(1.5 * screen.get_width() + 50)
 
+new_game(False)
+running = True
 while running:
     # events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
-    # gravity effects
+    # gravity effects and obstacle movement
     player.velocity += gravity
     player.coordinate.y += player.velocity
+    for obstacle in obstacles:
+        obstacle.addto_x(-5)
 
     # keys
     keys = pg.key.get_pressed()
@@ -100,18 +109,13 @@ while running:
         player.velocity = jump
 
     # draws
-    obstacle_x -= 5
-    obstacles = [
-        Obstacle(screen.get_width() + obstacle_x, 0, obstacle_width, obstacle_y),
-        Obstacle(screen.get_width() + obstacle_x, obstacle_y + hole_size, obstacle_width, screen.get_height())
-    ]
     screen.fill('black')
     player.draw(screen)
     obstacles[0].draw(screen)
     obstacles[1].draw(screen)
     pg.display.flip()
 
-    # bird death
+    # player's death
     for obstacle in obstacles:
         if player.touching(obstacle):
             new_game()
@@ -120,9 +124,10 @@ while running:
         new_game()
 
     # obstacle in front again
-    if obstacles[0].x2 < 0:
-        obstacle_y = rd.choice(range(0,450))
-        obstacle_x = 0
+    for obstacle in obstacles:
+        if obstacle.superior_pillar.right < 0:
+            obstacle.set_x(screen.get_width())
+            obstacle.set_y(rd.choice(range(0,450)))
 
     clock.tick(60)
 
